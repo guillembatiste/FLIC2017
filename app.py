84abf70f62f873df83a9f13502c1ce6869fdeaaf
Guillem <guillem.batiste@gmail.com>
2017-01-02 11:45:50 +0100

    Fixing packages issues

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import datetime as dt
 app = Flask(__name__)
 # -*- coding: utf-8 -*-
 from flask import Flask, request, render_template, abort
-from flask.ext.shelve import get_shelve, init_app
+from flask.shelve import get_shelve, init_app
 import datetime as dt
  
 app = Flask(__name__)
